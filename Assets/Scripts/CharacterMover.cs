726c02750d384966697660adf6c14ed215d1dd89
Teemu Kokkonen <teemu@rautasydan.org>
2019-11-06 11:17:01 +0200

    Platform switched to Android, build settings adjusted. Build OK. Player movement and touch controls.

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float MoveSpeed = 10.0f;      // Units per second
+    Transform _t;
+    private void Awake()
+    {
+        _t = GetComponent<Transform>();
+    }
+    public void Move(Vector2 dir)
+    {
+        // Since we're not using physics or rigidbodies, we can just use Transform's Translate()
+        _t.Translate(dir);
+    }
+
+    public void MoveTowards(Vector2 pos)
+    {
+        MoveTowards(pos, MoveSpeed * Time.deltaTime);
+    }
+    public void MoveTowards(Vector2 pos, float maxDistance)
+    {
+        _t.position = Vector2.MoveTowards(_t.position, pos, maxDistance);
+    }
+}
---
+++ Assets/Scripts/PlayerTouchController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMover))]
+public class PlayerTouchController : MonoBehaviour
+{
+    CharacterMover _mover;
+    Transform _t;
+    private void Awake()
+    {
+        _mover = GetComponent<CharacterMover>();
+        _t = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        { 
+            // Since MoveTowards() takes a Vector2, the z value is discarded and we don't have to worry about that
+            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, -Screen.width / 2, 0);
+            _mover.MoveTowards(targetPosition);     
+        }
+    }
+}
